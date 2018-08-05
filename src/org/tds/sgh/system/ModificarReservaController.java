35ab1b1b525d8161ce1dcc0817b604795a77534c
diego <diego@DESKTOP-AEJ38TA>
2018-08-04 20:53:17 -0400

    DTO Al modificarReserva Controler funcion ModificarReserva

--- src/org/tds/sgh/system/ModificarReservaController.java
+++ src/org/tds/sgh/system/ModificarReservaController.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import org.tds.sgh.business.*;
 import org.tds.sgh.dtos.ClienteDTO;
 import org.tds.sgh.dtos.HotelDTO;
+import org.tds.sgh.dtos.HuespedDTO;
 import org.tds.sgh.dtos.ReservaDTO;
 
 public class ModificarReservaController extends BaseController implements IModificarReservaController{
@@ -48,10 +49,38 @@ public class ModificarReservaController extends BaseController implements IModif
 	@Override
 	public ReservaDTO modificarReserva(String nombreHotel, String nombreTipoHabitacion, GregorianCalendar fechaInicio,
 			GregorianCalendar fechaFin, boolean modificablePorHuesped) throws Exception {
-		this.cadenaHotelera.modificarReserva(nombreHotel, nombreTipoHabitacion, fechaInicio, fechaFin, 
+		Reserva reserva = this.cadenaHotelera.modificarReserva(nombreHotel, 
+				nombreTipoHabitacion, 
+				fechaInicio, 
+				fechaFin, 
 				modificablePorHuesped);
+		int contador = 0;
+		HuespedDTO[] _huespedDTO = new HuespedDTO[reserva.getHuespedes().size()];
 		
-		return null;
+		Map<String, Huesped> map = reserva.getHuespedes();
+		
+		for (Map.Entry<String, Huesped> entry : map.entrySet())
+		{
+			
+			_huespedDTO[contador] = new HuespedDTO(entry.getKey(), entry.getValue().getNombre());
+			contador++;
+		}
+		
+		contador = 0;
+
+		ReservaDTO _reserva = new ReservaDTO(
+				reserva.getCodigo(),
+				reserva.getRutCliente().toString(),
+				reserva.getHotel().getNombre().toString(),
+				reserva.getTipoHabitacion().getNombre().toString(),
+				reserva.getFechaInicio(),
+				reserva.getFechaFin(),
+				reserva.getModificablePorHuesped(),
+				reserva.getEstado(),
+				reserva.getHabitacion().getNombre().toString(), 
+				_huespedDTO);
+				
+		return _reserva;
 	}
 
 	@Override
